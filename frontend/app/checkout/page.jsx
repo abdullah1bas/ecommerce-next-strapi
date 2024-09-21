3fca601ad215f6c0232b2469ac16ca04c7cbbd44
Abdullah Abazza <7a7abas@gmail.com>
2024-09-21 03:48:34 +0300

    mean of useSearchParams

--- frontend/app/checkout/page.jsx
+++ frontend/app/checkout/page.jsx
@@ -1,27 +1,27 @@
-'use client'
-import React from 'react'
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from './_components/CheckoutForm'
-import { useSearchParams } from 'next/navigation';
+"use client";
+import React from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./_components/CheckoutForm";
+import { useSearchParams } from "next/navigation";
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
 function Checkout() {
-	const searchParams = useSearchParams();
-	console.log(Number(searchParams.get('amount')) * 100);
-	// options = al 3omla , mode , amount 
-	const options = {
-		mode: 'payment',
-		currency: 'usd',
-		amount: Number(searchParams.get('amount')) * 100
-	}
-	return (
-		<Elements stripe={stripePromise} options={options}>
-			<CheckoutForm 
-			amount={Number(searchParams.get('amount'))} 
-			/>
-		</Elements>
-	)
+  // لتتعامل مع الـ URL query parameters (المعلمات الموجودة في عنوان الصفحة) في تطبيقات العميل.
+  // هذه Hook تتيح لك قراءة أو استخراج قيم معينة من الـ URL.
+  const searchParams = useSearchParams();
+  console.log(Number(searchParams.get("amount")) * 100);
+  // options = al 3omla , mode , amount
+  const options = {
+    mode: "payment",
+    currency: "usd",
+    amount: Number(searchParams.get("amount")) * 100,
+  };
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm amount={Number(searchParams.get("amount"))} />
+    </Elements>
+  );
 }
 
-export default Checkout
+export default Checkout;
